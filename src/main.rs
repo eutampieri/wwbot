51668e5f048a4b19d4cf7b8cb6bc69614dafaffe
Eugenio Tampieri <eugenio@eutampieri.eu>
2023-01-04 19:14:52 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use rand::seq::SliceRandom;
+use rand::seq::{IteratorRandom, SliceRandom};
 use rand::thread_rng;
 
 fn main() {
     //let topics: Vec<Vec<String>> = bincode::deserialize(include_bytes!("../topics.bin")).unwrap();
     let topics: Vec<Vec<String>> = serde_json::from_str(include_str!("../topics.json")).unwrap();
     let mut rng = thread_rng();
-    println!(
-        "{:?}",
-        topics
-            .choose(&mut rng)
-            .expect("Empty topics")
-            .choose_multiple(&mut rng, 2)
-    );
+    let chosen = {
+        let mut t = vec![];
+        while t.len() != 2 {
+            t = topics
+                .choose(&mut rng)
+                .expect("Empty topics")
+                .iter()
+                .filter(|x| x.len() < 18)
+                .choose_multiple(&mut rng, 2);
+        }
+        t
+    };
+    println!("{:?}", chosen);
 }
